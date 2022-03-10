4d6d4280f1fcaa84e253560fe36f980d72a773a5
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-03-10 17:42:17 +0700

    feat: update solution

--- solutions/naufal/generateChildrenName.js
+++ solutions/naufal/generateChildrenName.js
@@ -1,5 +1,5 @@
 function generateChildrenName(characters, name) {
-  return name.split("").filter(x => characters.indexOf(x) === -1).join("") ? false : true;
+  return name.split("").filter(function(char) { return characters.split("").find(function(c, i) {if(this.temp.includes(i) || (c !== char)) { return false; } else { this.temp.push(i); return true; } }, this)}, { temp:[] }).join("") === name
 }
 
 // dont change the code below
